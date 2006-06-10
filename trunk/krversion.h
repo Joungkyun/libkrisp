e176ca9e1e28f9278cbbe7ad30da4293cf215af6
oops <oops@97d1dff6-65cd-11e0-bb5f-87b7cc9bdab0>
2006-06-10 12:13:51 +0000

    defined VERSION_H

--- trunk/krversion.h
+++ trunk/krversion.h
@@ -1,9 +1,14 @@
 /*
- * $Id: krversion.h,v 1.1 2006-06-10 12:13:02 oops Exp $
+ * $Id: krversion.h,v 1.2 2006-06-10 12:13:51 oops Exp $
  */
+
+#ifndef VERSION_H
+#define VERSION_H
 
 #define VERSION "0.0.3"
 #define UVERSION "000000003"
+
+#endif
 
 /*
  * Local variables:
