835646bdcb934dabbbe6b6f4056b9bb429fc49d6
JoungKyun Kim <joungkyun@daum.net>
2006-06-13 08:43:02 +0000

    start 0.0.2

--- krversion.h
+++ krversion.h
@@ -1,12 +1,12 @@
 /*
- * $Id: krversion.h,v 1.4 2006-06-12 16:17:01 oops Exp $
+ * $Id: krversion.h,v 1.5 2006-06-13 08:43:02 oops Exp $
  */
 
 #ifndef KR_VERSION_H
 #define KR_VERSION_H
 
-#define KRISP_VERSION "1.0.1"
-#define KRISP_UVERSION "001000001"
+#define KRISP_VERSION "1.0.2"
+#define KRISP_UVERSION "001000002"
 
 #endif
 
