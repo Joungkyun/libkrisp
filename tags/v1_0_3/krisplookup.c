58cb945001c8a3e27be19205853ace00a53dd6be
nobody <nobody@97d1dff6-65cd-11e0-bb5f-87b7cc9bdab0>
2006-06-18 16:04:20 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_0_3'.

---
+++ tags/v1_0_3/krisplookup.c
@@ -0,0 +1,117 @@
+/*
+ * $Id: krisplookup.c,v 1.16 2006-06-13 08:53:57 oops Exp $
+ */
+
+#include <krisp.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
+
+#define PNAME "krisplookup"
+
+#ifdef HAVE_GETOPT_LONG
+static struct option long_options [] = {
+	/* Options without arguments: */
+	{ "help", no_argument, NULL, 'h' },
+
+	/* Options accepting an argument: */
+	{ "datafile", required_argument, NULL, 'f' },
+	{ 0, 0, 0, 0 }
+};
+#endif
+
+extern char dberr[1024];
+
+void usage (char *prog) {
+	fprintf (stderr, "%s v%s: Resolved korea range ISP inforamtion\n", prog, krisp_version ());
+	fprintf (stderr, "Usage: %s [option] ip-address\n", prog);
+	fprintf (stderr, "Options:\n");
+	fprintf (stderr, "         -f path, --datafile=path     set user define database file\n");
+	fprintf (stderr, "         -h , --help                  print this message\n\n");
+
+	exit (1);
+}
+
+int main (int argc, char ** argv) {
+	struct db_argument db;
+	struct netinfos isp;
+	struct stat f;
+	char * ip;
+	int opt;
+	char *datafile = NULL;
+
+#ifdef HAVE_GETOPT_LONG
+	while ( (opt = getopt_long (argc, argv, "f:h", long_options, (int *) 0)) != EOF ) {
+#else
+	while ( (opt = getopt (argc, argv, "f:h")) != EOF ) {
+#endif
+		switch (opt) {
+			case 'f' :
+				datafile = optarg;
+				break;
+			default:
+				usage (PNAME);
+		}
+	}
+
+	if ( argc - optind < 1 || argc == 1 ) {
+		usage (PNAME);
+		return 1;
+	}
+
+	f.st_size = 0;
+	if ( stat ((datafile != NULL) ? datafile : DBPATH, &f) == -1 ) {
+		fprintf (stderr, "ERROR: Can't find data file (%s)\n",
+				(datafile != NULL) ? datafile : DBPATH);
+		return 1;
+	}
+
+	if ( f.st_size < 1 ) {
+		fprintf (stderr, "ERROR: %s size is zero\n",
+				(datafile != NULL) ? datafile : DBPATH);
+		return 1;
+	}
+
+	/* database open */
+	if ( kr_open (&db, (datafile != NULL) ? datafile : NULL) ) {
+		fprintf (stderr, "ERROR Connect: %s\n", dberr);
+		return 1;
+	}
+
+	ip = argv[optind];
+
+#ifdef HAVE_LIBGEOIP
+	/*
+	 * If you don't want to use geoip, set 'gi = NULL'.
+	 */
+	db.gi = GeoIP_new (GEOIP_MEMORY_CACHE);
+#else
+	db.gi = NULL;
+#endif
+
+	if ( strlen (ip) > 255 ) {
+		strncpy (isp.ip, ip, 255);
+		isp.ip[255] = 0;
+	} else {
+		strcpy (isp.ip, ip);
+	}
+	kr_search (&isp, &db);
+
+	printf ("%s (%s): %s (%s)\n", ip, isp.ip, isp.org, isp.serv);
+	printf ("SUBNET : %s\n", isp.netmask);
+	printf ("NETWORK: %s\n", isp.network);
+	printf ("BCAST  : %s\n", isp.broadcast);
+
+#ifdef HAVE_LIBGEOIP
+	printf ("NATION : %s (%s)\n", isp.nation, isp.code);
+#endif
+
+	/* database close */
+	kr_close (&db);
+
+	return 0;
+}
---
+++ tags/v1_0_3/krversion.h
@@ -0,0 +1,20 @@
+/*
+ * $Id: krversion.h,v 1.6 2006-06-16 03:00:58 oops Exp $
+ */
+
+#ifndef KR_VERSION_H
+#define KR_VERSION_H
+
+#define KRISP_VERSION "1.0.3"
+#define KRISP_UVERSION "001000003"
+
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
