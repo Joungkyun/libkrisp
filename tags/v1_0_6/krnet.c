0515ad8bc8051ba713002c07c2809c5573f21769
nobody <nobody@97d1dff6-65cd-11e0-bb5f-87b7cc9bdab0>
2006-09-04 03:49:43 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_0_6'.

---
+++ tags/v1_0_6/krnet.c
@@ -0,0 +1,82 @@
+/*
+ * $Id: krnet.c,v 1.4 2006-06-20 03:32:11 oops Exp $
+ */
+
+#include <krispcommon.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <krnet.h>
+
+/*
+ * Converts a string containing an (IPv4) Internet Protocol dotted address into a proper address
+ */
+unsigned long ip2long (char *ip) {
+	if ( ! strlen (ip) || inet_addr (ip) == INADDR_NONE ) {
+		return -1;
+	}
+
+	return ntohl (inet_addr (ip));
+}
+
+/*
+ * Converts IPv4 address into a string in Internet standard dotted format
+ */
+char * long2ip (unsigned long num) {
+	struct in_addr addr;
+
+	/*
+	* if num is charactor type, add follow line
+	* n = strtoul (num, NULL, 0);
+	*/
+	addr.s_addr = htonl (num);
+
+	return inet_ntoa (addr);
+}
+
+int makeNetworkInfo (char *ip, KRNET_API *n) {
+	struct in_addr addr, mddr, tddr;
+	int tmp;
+
+	if ( ! inet_aton (ip, &addr) )
+		return 1;
+
+	// input ip
+	strcpy (n->ip, ip);
+
+	addr.s_addr = ntohl (addr.s_addr);
+
+	// make subnet mask
+	if ( ((addr.s_addr & 0xFF000000) >> 24) <= 127 )
+		strcpy (n->netmask, "255.0.0.0");
+	else if (((addr.s_addr & 0xFF000000) >> 24) <= 191)
+		strcpy (n->netmask, "255.255.0.0");
+	else
+		strcpy (n->netmask, "255.255.255.0");
+
+	// make network
+	if ( ! inet_aton (n->netmask, &mddr) )
+		return 1;
+
+	addr.s_addr = htonl (addr.s_addr);
+
+	tmp = addr.s_addr & mddr.s_addr;
+	tddr.s_addr = htonl (ntohl (tmp));
+	strcpy (n->network, inet_ntoa (tddr));
+
+	// make broadcast
+	tmp = (addr.s_addr & mddr.s_addr) | (~mddr.s_addr);
+	tddr.s_addr = htonl (ntohl (tmp));
+	strcpy (n->broadcast, inet_ntoa (tddr));
+
+	return 0;
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
---
+++ tags/v1_0_6/krversion.h
@@ -0,0 +1,20 @@
+/*
+ * $Id: krversion.h,v 1.9 2006-06-22 05:45:54 oops Exp $
+ */
+
+#ifndef KR_VERSION_H
+#define KR_VERSION_H
+
+#define KRISP_VERSION "1.0.6"
+#define KRISP_UVERSION "001000006"
+
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
