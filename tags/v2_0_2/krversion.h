c87bd068301d726ba5786d53d42a8fdec6017a86
nobody <nobody@97d1dff6-65cd-11e0-bb5f-87b7cc9bdab0>
2006-09-14 19:41:20 +0000

    This commit was manufactured by cvs2svn to create tag 'v2_0_2'.

---
+++ tags/v2_0_2/krversion.h
@@ -0,0 +1,20 @@
+/*
+ * $Id: krversion.h,v 1.12 2006-09-13 13:38:57 oops Exp $
+ */
+
+#ifndef KR_VERSION_H
+#define KR_VERSION_H
+
+#define KRISP_VERSION "2.0.2"
+#define KRISP_UVERSION "002000002"
+
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
