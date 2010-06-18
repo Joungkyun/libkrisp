9fe64d44ddd29378ae8539115e2895c4c0ec7130
JoungKyun Kim <joungkyun@daum.net>
2010-06-18 13:07:14 +0000

    optimize structure padding

--- krispcommon.h
+++ krispcommon.h
@@ -1,5 +1,5 @@
 /*
- * $Id: krispcommon.h,v 1.33 2010-06-18 12:59:01 oops Exp $
+ * $Id: krispcommon.h,v 1.34 2010-06-18 13:07:14 oops Exp $
  */
 
 #ifndef KR_COMMON_H
@@ -40,6 +40,7 @@ typedef enum {
 #define switch_bool(x) ((x == true) ? false : true)
 
 typedef struct db_argument {
+	char 			err[1024];
 #if defined(HAVE_LIBSQLITE3)
 	sqlite3	*		c;		// db resource
 	sqlite3_stmt *	vm;		// sqlite vm
@@ -47,48 +48,47 @@ typedef struct db_argument {
 	sqlite *		c;		// db resource
 	sqlite_vm *		vm;		// sqlite vm
 #endif
+	const char **	rowdata;
+	const char **	colname;
+	char *			table;
+	char *			old_table;
+#if defined(HAVE_LIBSQLITE)
+	char *			dberr;	// vm error message
+#endif
+	int				rows;	// vm rows
+	int				cols;	// number of columns
 	bool			verbose;
 #ifdef HAVE_PTHREAD_H
 	bool			threadsafe;
 #endif
 	short			r;		// execute result code
 	short			final;  // force finalize
-	int				rows;	// vm rows
-	int				cols;	// number of columns
-#if defined(HAVE_LIBSQLITE)
-	char *			dberr;	// vm error message
-#endif
-	char 			err[1024];
-	char *			table;
-	char *			old_table;
-	const char **	rowdata;
-	const char **	colname;
 } KR_API;
 
 typedef struct raw_netinfos {
-	bool			verbose;
-	char			ip[16];
-	ulong			start;
-	ulong			end;
-	short			size;
 	char			err[1024];
+	char			ip[16];
 	char **			dummy;
 	char *			dummydata;
+	ulong			start;
+	ulong			end;
+	bool			verbose;
+	short			size;
 } RAW_KRNET_API;
 
 #define KRNET_API_EX RAW_KRNET_API
 
 typedef struct netinfos {
-	bool			verbose;
+	char			err[1024];
 	char			ip[256];
+	char			icode[64];
+	char			iname[64];
+	char			cname[64];
+	char			ccode[4];
 	ulong			netmask;
 	ulong			start;
 	ulong			end;
-	char			icode[64];
-	char			iname[64];
-	char			ccode[4];
-	char			cname[64];
-	char			err[1024];
+	bool			verbose;
 } KRNET_API;
 
 struct cinfo {
