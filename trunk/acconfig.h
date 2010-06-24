fb4c76683124757088eed77c42e576ea4fda01f0
oops <oops@97d1dff6-65cd-11e0-bb5f-87b7cc9bdab0>
2010-06-24 17:53:17 +0000

    refixed redefine PACKAGE_* constants

--- trunk/acconfig.h
+++ trunk/acconfig.h
@@ -1,25 +1,25 @@
-/* $Id: acconfig.h,v 1.2 2010-06-24 17:36:08 oops Exp $ */
+/* $Id: acconfig.h,v 1.3 2010-06-24 17:53:17 oops Exp $ */
 #ifndef KRISP_CONFIG_H
 #define KRISP_CONFIG_H
 
 #ifdef PACKAGE_BUGREPORT
-    #undef PACKAGE_BUGREPORT
+    #undef KR_PACKAGE_BUGREPORT
 #endif
 
 #ifdef PACKAGE_NAME
-    #undef PACKAGE_NAME
+    #undef KR_PACKAGE_NAME
 #endif
 
 #ifdef PACKAGE_STRING
-    #undef PACKAGE_STRING
+    #undef KR_PACKAGE_STRING
 #endif
 
 #ifdef PACKAGE_TARNAME
-    #undef PACKAGE_TARNAME
+    #undef KR_PACKAGE_TARNAME
 #endif
 
 #ifdef PACKAGE_VERSION
-    #undef PACKAGE_VERSION
+    #undef KR_PACKAGE_VERSION
 #endif
 
 
