cfe045be00a88f8d5e5448662581b981a1101e05
JoungKyun Kim <joungkyun@daum.net>
2010-06-24 17:36:08 +0000

    fixed redefine PACKAGE_* constants

--- acconfig.h
+++ acconfig.h
@@ -1,6 +1,26 @@
-/* $Id: acconfig.h,v 1.1.1.1 2006-06-08 19:31:52 oops Exp $ */
+/* $Id: acconfig.h,v 1.2 2010-06-24 17:36:08 oops Exp $ */
 #ifndef KRISP_CONFIG_H
 #define KRISP_CONFIG_H
+
+#ifdef PACKAGE_BUGREPORT
+    #undef PACKAGE_BUGREPORT
+#endif
+
+#ifdef PACKAGE_NAME
+    #undef PACKAGE_NAME
+#endif
+
+#ifdef PACKAGE_STRING
+    #undef PACKAGE_STRING
+#endif
+
+#ifdef PACKAGE_TARNAME
+    #undef PACKAGE_TARNAME
+#endif
+
+#ifdef PACKAGE_VERSION
+    #undef PACKAGE_VERSION
+#endif
 
 
 /* config.h:  a general config file */
