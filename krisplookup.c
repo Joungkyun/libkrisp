6e26a907256ed09e11f7d4a36aff3f4cf33529ec
JoungKyun Kim <joungkyun@daum.net>
2010-09-10 13:23:24 +0000

    don't check database modification

--- krisplookup.c
+++ krisplookup.c
@@ -1,5 +1,5 @@
 /*
- * $Id: krisplookup.c,v 1.51 2010-09-10 12:44:25 oops Exp $
+ * $Id: krisplookup.c,v 1.52 2010-09-10 13:23:24 oops Exp $
  */
 
 #include <krisp.h>
@@ -137,6 +137,8 @@ int main (int argc, char ** argv) {
 		fprintf (stderr, "ERROR Connect: %s\n", err);
 		return 1;
 	}
+
+	db->db_time_stamp_interval = 0;
 
 	isp.verbose = verbose;
 	db->verbose = verbose;
