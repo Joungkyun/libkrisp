c20c8dcdc8f593b96bf831cc601cff30fc83234b
oops <oops@97d1dff6-65cd-11e0-bb5f-87b7cc9bdab0>
2010-09-10 08:58:03 +0000

    version update to 3.0.3

--- trunk/krversion.h
+++ trunk/krversion.h
@@ -1,12 +1,12 @@
 /*
- * $Id: krversion.h,v 1.23 2010-09-10 08:55:21 oops Exp $
+ * $Id: krversion.h,v 1.24 2010-09-10 08:58:03 oops Exp $
  */
 
 #ifndef KR_VERSION_H
 #define KR_VERSION_H
 
-#define KRISP_VERSION "3.0.2"
-#define KRISP_UVERSION "003000002"
+#define KRISP_VERSION "3.0.3"
+#define KRISP_UVERSION "003000003"
 
 #endif
 
