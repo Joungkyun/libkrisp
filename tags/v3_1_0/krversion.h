ad8521891303536e0e0f9e4c5d29413e3c22c37a
nobody <nobody@97d1dff6-65cd-11e0-bb5f-87b7cc9bdab0>
2010-09-10 13:19:19 +0000

    This commit was manufactured by cvs2svn to create tag 'v3_1_0'.

---
+++ tags/v3_1_0/krversion.h
@@ -0,0 +1,20 @@
+/*
+ * $Id: krversion.h,v 1.25 2010-09-10 10:58:33 oops Exp $
+ */
+
+#ifndef KR_VERSION_H
+#define KR_VERSION_H
+
+#define KRISP_VERSION "3.1.0"
+#define KRISP_UVERSION "003001000"
+
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
---
+++ tags/v3_1_0/test/thread_sample2.c
@@ -0,0 +1,83 @@
+/*
+ * $Id: thread_sample2.c,v 1.16 2010-09-10 12:44:26 oops Exp $
+ */
+
+#include <krisp.h>
+#include <sys/time.h>
+
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
+
+#define THREAD_SIZE 5
+
+pthread_t threads[THREAD_SIZE];
+
+void * thread_main (void *);
+ulong prand (void);
+
+int main (void) { // {{{
+	int		i, r;
+	long	status;
+
+	for ( i=0; i<THREAD_SIZE; i++ ) {
+		pthread_create (&threads[i], NULL, &thread_main, (void *) i);
+		printf ("%d, %ld\n", i, threads[i]);
+	}
+
+	for ( i=0; i<THREAD_SIZE; i++ ) {
+		r = pthread_join (threads[i], (void **) &status);
+		if ( r == 0 )
+			printf ("Completed join with thread %d status=%d\n",i, (int) status);
+		else
+			printf ("ERROR; return code from pthread_join() is %d, thread %d\n", r, i);
+	}
+
+	return 0;
+} // }}}
+
+void * thread_main (void * arg) { // {{{
+	int			tno;
+	char *		ip;
+	KR_API *	db;
+	KRNET_API	isp;
+	char		err[1024];
+
+	tno = (int) arg;
+
+	ip = long2ip (prand ());
+
+	/* database open */
+	if ( kr_open (&db, NULL, err) == false ) {
+		fprintf (stderr, "ERROR Connect: %s\n", err);
+		kr_close (&db);
+		pthread_exit ((void *) 0);
+	}
+
+	isp.verbose = false;
+	SAFECPY_256 (isp.ip, ip);
+
+	if ( kr_search (&isp, db) ) {
+		fprintf (stderr, "ERROR: %s\n", isp.err);
+	} else
+		printf ("--> Thread %d : %15s => %s\n", tno, isp.ip, isp.icode);
+
+	kr_close (&db);
+	pthread_exit ((void *) 0);
+} // }}}
+
+ulong prand (void) { // {{{
+	int c;
+	ulong p;
+
+retry:
+	c = time (0) % 2;
+	p = random ();
+	if ( c == 0 )
+		p *= 2;
+
+	if ( p < 16777216 )
+		goto retry;
+
+	return p;
+} // }}}
