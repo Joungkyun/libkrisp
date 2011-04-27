3d44dfef58db9952d879a9bff7b7eff4eaa4d41d
JoungKyun Kim <joungkyun@daum.net>
2011-04-27 07:27:49 +0000

    rename tag name

--- krisp.h
+++ krisp.h
@@ -1,55 +1,79 @@
 /*
- * $Id: krisp.h,v 1.30 2010-09-10 11:09:14 oops Exp $
+ * $Id: krisp.h,v 1.11 2006-06-20 03:32:11 oops Exp $
  */
 
-#ifndef KR_ISP_H
-#define KR_ISP_H
+#ifndef KRIP_H
+#define KRIP_H
 
+#include <krversion.h>
 #include <krispcommon.h>
+#include <krdb.h>
 
 /*
- * KRISP user level function prototype
+ *
+ * user level function prototype
+ *
  */
-
-/*
- * Init information structure
- */
-extern void initStruct (KRNET_API *);
-
-/*
- * Init raw database data structure
- */
-extern void initRawStruct (RAW_KRNET_API *, bool);
-#define initStruct_ex initRawStruct
 
 /*
  * print krisp version
  */
-extern char * krisp_version (void);
-extern char * krisp_uversion (void);
+char *krisp_version (void);
+char *krisp_uversion (void);
 
 /*
  * open krisp database
- * int kr_open (KR_API **db, char *database, char *err);
  */
-extern bool kr_open (KR_API **, char *, char *);
-extern bool kr_open_safe (KR_API **, char *, char *);
+int kr_open (KR_API *, char *);
 
 /*
  * close krisp database
  */
-extern void kr_close (KR_API **);
+void kr_close (KR_API *);
 
 /*
  * search isp information of given ip address
  */
-extern int kr_search (KRNET_API *, KR_API *);
-extern int kr_search_ex (KRNET_API_EX *, KR_API *);
+int kr_search (KRNET_API *isp, KR_API *db);
 
-extern void krisp_safecpy (char *, char *, int);
-#define SAFECPY_256(dest, src) krisp_safecpy(dest, src, 256)
-#define SAFECPY_512(dest, src) krisp_safecpy(dest, src, 512)
-#define SAFECPY_1024(dest, src) krisp_safecpy(dest, src, 1024)
+
+
+/* -------------------{{{ not use on user level---------------------- */
+
+
+/*
+ *
+ * not use on user level
+ *
+ */
+
+/*
+ * get netmask of each A class ip from db
+ */
+int kr_netmask (KR_API *, char *, struct netmasks *);
+
+/*
+ * get ISP information of each network from db
+ */
+int getISPinfo (KR_API *, char *, KRNET_API *);
+
+/*
+ * free memory that used db query function
+ */
+void kr_free_array (char **);
+
+/*
+ * init information structure
+ */
+void initStruct (KRNET_API *n);
+
+/*
+ * convert ip address to netlong type
+ */
+unsigned long ip2long (char *ip);
+
+/* }}} */
+
 #endif
 
 /*
