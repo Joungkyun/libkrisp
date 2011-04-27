f1d52c2c40c7e2bd504194d9fd0e61ad8b6fc4d5
JoungKyun Kim <joungkyun@daum.net>
2011-04-27 07:27:49 +0000

    rename tag name

--- acconfig.h
+++ acconfig.h
@@ -1,21 +1,12 @@
-/* $Id: acconfig.h,v 1.5 2010-11-08 06:38:58 oops Exp $ */
+/* $Id: acconfig.h,v 1.1.1.1 2006-06-08 19:31:52 oops Exp $ */
 #ifndef KRISP_CONFIG_H
 #define KRISP_CONFIG_H
+
 
 /* config.h:  a general config file */
 
 @TOP@
 
 @BOTTOM@
-
-/*
- *  * Compotable C Type
- *   */
-
-#if ! defined C_ULONG_DEFINED
-#	if ! defined ulong
-		typedef unsigned long   ulong;
-#	endif
-#endif
 
 #endif /*  KRISP_CONFIG_H */
--- krversion.h
+++ krversion.h
@@ -1,12 +1,12 @@
 /*
- * $Id: krversion.h,v 1.28 2011-01-14 05:08:35 oops Exp $
+ * $Id: krversion.h,v 1.15 2006-12-07 06:42:21 oops Exp $
  */
 
 #ifndef KR_VERSION_H
 #define KR_VERSION_H
 
-#define KRISP_VERSION "3.1.3"
-#define KRISP_UVERSION "003001003"
+#define KRISP_VERSION "2.1.1"
+#define KRISP_UVERSION "002001001"
 
 #endif
 
