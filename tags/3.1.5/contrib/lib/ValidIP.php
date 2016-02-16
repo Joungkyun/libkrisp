01bd22f10ed1045e1de11c7297276836f0b0265e
oops <oops@97d1dff6-65cd-11e0-bb5f-87b7cc9bdab0>
2016-02-16 18:37:58 +0000

    tagging 3.1.5

---
+++ tags/3.1.5/contrib/lib/ValidIP.php
@@ -0,0 +1,134 @@
+<?php
+/*
+ * $Id$
+ *
+ * Validate IP range
+ */
+
+Class ValidIP {
+	function valid ($v) { // {{{
+		if ( preg_match ('/[0-9]{1,3}\.([0-9]{1,3}){3}$/', $v) )
+			$ip = self::ip2long ($v);
+		else if ( strstr ($v, '.') !== false ) {
+			$ip = gethostbyname ($v);
+			if ( $ip === false )
+				return false;
+			$ip = self::ip2long ($ip);
+		} else
+			$ip = $v;
+
+		if ( $ip === false )
+			return false;
+
+		return self::valid_longip ($ip);
+	} // }}}
+
+	function valid_longip ($v) { // {{{
+		//if ( $v < 16777216 || $v > 4294967295 )
+		if ( $v < 16777216 || $v >= 4026531840 )
+			return false;
+
+		if ( self::is_loopback ($v) === true )
+			return false;
+
+		if ( self::is_private_addr ($v) === true )
+			return false;
+
+		if ( self::is_multicast ($v) === true )
+			return false;
+
+		if ( self::is_eclass ($v) === true )
+			return false;
+
+		if ( self::is_reserved ($v) === true )
+			return false;
+
+		return true;
+	} // }}}
+
+	function is_reserved ($v) { // {{{
+		// 169.254.0.0/16 reserved for Link Local [RFC5735]
+		if ( $v >= 2851995648 && $v <= 2852061183 )
+			return true;
+
+		// 192.0.0.0/24 reserved for IANA IPv4 Special Purpose Address Registry [RFC5736]
+		if ( $v >= 3221225472 && $v <= 3221225727 )
+			return true;
+
+		// 192.0.2.0/24  reserved for TEST-NET-1 [RFC5737]
+		if ( $v >= 3221225984 && $v <= 3221226239 )
+			return true;
+
+		// 198.18.0.0/15 reserved for Network Interconnect Device Benchmark Testing [RFC5735]
+		if ( $v >= 3323068416 && $v <= 3323199487 )
+			return true;
+
+		// 198.51.100.0/24 reserved for TEST-NET-2 [RFC5737]
+		if ( $v >= 3325256704 && $v <= 3325256959 )
+			return true;
+
+		// 192.88.99.0/24 reserved for 6to4 Relay Anycast [RFC3068]
+		if ( $v >= 3227017984 && $v <= 3227018239 )
+			return true;
+
+		// 203.0.113.0/24 reserved for TEST-NET-3 [RFC5737]
+		if ( $v >= 3405803776 && $v <= 3405804031 )
+			return true;
+
+		return false;
+	} // }}}
+
+	function is_loopback ($v) { // {{{
+		if ( $v >= 2130706432 && $v <= 2147483647 )
+			return true;
+		return false;
+	} // }}}
+
+	function is_private_addr ($v) { // {{{
+		// A class 10.0.0.0 - 10.255.255.255
+		if ( $v >= 167772160 && $v <= 184549375 )
+			return true;
+
+		// B class 172.16.0.0 - 172.31.0.0
+		if ( $v >= 2886729728 && $v <= 2887712768 )
+			return true;
+
+		// C class 192.168.0.0 - 192.168.255.255
+		if ( $v >= 3232235520 && $v <= 3232301055 )
+			return true;
+
+		return false;
+	} // }}}
+
+	function is_multicast ($v) { // {{{
+		// 224.0.0.0 - 239.255.255.0
+		if ( $v >= 3758096384 && $v <= 4026531839 )
+			return true;
+
+		return false;
+	} // }}}
+
+	function is_eclass ($v) { // {{{
+		if ( $v >= 4026531840 && $v <= 4294967295 )
+			return true;
+
+		return false;
+	} // }}}
+
+	function ip2long ($v) { // {{{
+		$longip = ip2long ($v);
+		if ( $longip === false )
+			return '';
+		return sprintf ('%u', $longip);
+	} // }}}
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
+?>
---
+++ tags/3.1.5/krisp.h
@@ -0,0 +1,80 @@
+/*
+ * $Id$
+ */
+
+#ifndef KR_ISP_H
+#define KR_ISP_H
+
+#include <krversion.h>
+#if defined _WIN32  && ! defined HAVE_CONFIG_H
+#include <krispcommon.h.in>
+#else
+#include <krispcommon.h>
+#endif
+
+/*
+ * KRISP user level function prototype
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Init information structure
+ */
+extern void initStruct (KRNET_API *);
+
+/*
+ * Init raw database data structure
+ */
+extern void initRawStruct (RAW_KRNET_API *, bool);
+#define initStruct_ex initRawStruct
+
+/*
+ * print krisp version
+ */
+extern char * krisp_version (void);
+extern char * krisp_uversion (void);
+
+/*
+ * open krisp database
+ * int kr_open (KR_API **db, char *database, char *err);
+ */
+extern bool kr_open (KR_API **, char *, char *);
+extern bool kr_open_safe (KR_API **, char *, char *);
+
+/*
+ * close krisp database
+ */
+extern void kr_close (KR_API **);
+
+/*
+ * search isp information of given ip address
+ */
+extern int kr_search (KRNET_API *, KR_API *);
+extern int kr_search_ex (KRNET_API_EX *, KR_API *);
+extern int kr_range (KRNET_REQ_RANGE *, KR_API *);
+
+extern void krisp_safecpy (char *, char *, int);
+#define SAFECPY_256(dest, src) krisp_safecpy(dest, src, 256)
+#define SAFECPY_512(dest, src) krisp_safecpy(dest, src, 512)
+#define SAFECPY_1024(dest, src) krisp_safecpy(dest, src, 1024)
+#ifndef SAFEFREE
+	#define	SAFEFREE(x) if ( x != NULL ) { free (x); x = NULL; }
+#endif
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
